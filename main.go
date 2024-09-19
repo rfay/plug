a1d4445b3383210d08a38bac407f9047c8921606
Randy Fay <randy@randyfay.com>
2024-09-18 18:33:26 -0600

    Revert "starting to compile"
    This reverts commit 3f5111ee175e8c2710585eb5a2befb20918c276d.

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"os"
-	"reflect"
-
 	"github.com/traefik/yaegi/interp"
 	"github.com/traefik/yaegi/stdlib"
-
-	yaml "gopkg.in/yaml.v3"
+	// Import the extracted symbols package
 )
 
 func main() {
@@ -16,37 +12,19 @@ func main() {
 	// Use the standard library symbols
 	i.Use(stdlib.Symbols)
 
-	// Register the symbols from the yaml package
-	i.Use(map[string]map[string]reflect.Value{
-		"gopkg.in/yaml.v3": {
-			"Marshal":    reflect.ValueOf(yaml.Marshal),
-			"Unmarshal":  reflect.ValueOf(yaml.Unmarshal),
-			"NewDecoder": reflect.ValueOf(yaml.NewDecoder),
-			"NewEncoder": reflect.ValueOf(yaml.NewEncoder),
-			// Add other functions or types if needed
-		},
-	})
-
-	// Register the os package symbols if needed
-	i.Use(map[string]map[string]reflect.Value{
-		"os": {
-			"ReadFile": reflect.ValueOf(os.ReadFile),
-			"Args":     reflect.ValueOf(os.Args),
-			// Add other functions if needed
-		},
-	})
+	// Use the extracted yaml symbols
+	i.Use(yaml_yaegi.Symbols)
 
 	// The code to be interpreted
 	src := `
         package main
 
         import (
-            "os"
             yaml "gopkg.in/yaml.v3"
         )
 
         func main() {
-            data, err := os.ReadFile("junk.yaml")
+            data, err := ioutil.ReadFile("junk.yaml")
             if err != nil {
                 panic(err)
             }
@@ -59,7 +37,7 @@ func main() {
 
             // Traverse the map
             for k, v := range m {
-                println(k, ":", v)
+                fmt.Printf("%s: %v\n", k, v)
             }
         }
     `
